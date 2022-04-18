828a4e9ecaca09b3cf9a56738a1780523262c853
David <drv404@hotmail.com>
2022-04-18 14:27:18 -0500

    first commit er

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur quas sapiente cum esse ullam quam facere temporibus pariatur. Amet laudantium quaerat eius voluptatem placeat nisi odio beatae minima voluptatibus! Odio?
+      </p>
+    </div>
+  )
+}
+
+export default Home;
